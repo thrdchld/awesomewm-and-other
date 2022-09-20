bfcf1a8d7b4a61fa3f2e55093e7c3f8ada95c719
thrdchld <109644096+thrdchld@users.noreply.github.com>
2022-09-20 10:20:52 +0700

    Add files via upload

---
+++ dotconfig/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fish_vi_key_bindings
+end
+
+/* for mode in default insert visual */
+  /* bind -M $mode \r -m default execute */
+/* end */
